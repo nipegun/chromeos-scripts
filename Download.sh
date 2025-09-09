7ec03ca094347b53385578c1f45095ce6cf06b1a
NiPeGun <nipegun@gmail.com>
2025-09-09 09:44:46 +0200

    Create Download.sh

---
+++ Download.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# I make this script publicly available under the term "public domain software."
+# You can do whatever you want with it because it is truly free—unlike so-called "free" software with conditions, like the GNU licenses and other similar nonsense.
+# If you're so eager to talk about freedom, then make it truly free.
+# You don't have to accept any terms of use or license to use or modify it, because it comes with no CopyLeft.
+
+# ----------
+# NiPeGun's script to download the ChromeOS ZIP recovery file
+#
+# Remote execution (may require sudo privileges):
+#   curl -sL https://raw.githubusercontent.com/nipegun/chromeos-scripts/refs/heads/main/ZIPFile-Download.sh | bash
+#
+# Remote execution as root (for systems without sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/chromeos-scripts/refs/heads/main/ZIPFile-Download.sh | sed 's-sudo--g' | bash
+#
+# Download and edit the file directly in nano:
+#   curl -sL https://raw.githubusercontent.com/nipegun/chromeos-scripts/refs/heads/main/ZIPFile-Download.sh | nano -
+
+# Set the -zip file URL
+  vFileURL="https://dl.google.com/chromeos-flex/images/latest.bin.zip"
+
+# Downlaod
+  echo ""
+  echo "  Downloading .zip file..."
+  echo ""
+  # Check if curl package is installed. If not, install ir.
+    if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}  The curl package is not installed. Starting its installation...${cFinColor}"
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install curl
+      echo ""
+    fi
+  cd /tmp
+  curl -L $vFileURL -o /tmp/chromeos-flex.zip --progress-bar
+
